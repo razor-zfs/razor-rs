8412b56129c9fd1776b661c4dd337f77069d31a1
Cyril Plisko <cyril.plisko@replix.io>
2021-03-10 16:40:18 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![cfg_attr(feature = "pedantic", warn(clippy::pedantic))]
+#![warn(clippy::use_self)]
+#![warn(deprecated_in_future)]
+#![warn(future_incompatible)]
+#![warn(unreachable_pub)]
+#![warn(missing_debug_implementations)]
+#![warn(rust_2018_compatibility)]
+#![warn(rust_2018_idioms)]
+#![warn(unused)]
+#![deny(warnings)]
+
+fn main() {
+    println!("razor {}", env!("CARGO_PKG_VERSION"));
+}
