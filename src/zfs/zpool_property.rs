33468245cf738a4a04658bf57f26bc0fa0f57766
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-07-04 18:35:10 +0300

    changed yes|no to on|off in zpool properties

--- src/zfs/zpool_property.rs
+++ src/zfs/zpool_property.rs
@@ -9,19 +9,18 @@ use ashift::Ashift as AshiftSectorSize;
 use failmode::Failmode as FailmodeStatus;
 use health::Health as HealthStatus;
 pub use onoff::OnOff;
-use yesno::YesNo;
 
 pub type Allocated = Property<String>;
 pub type Altroot = Property<String>;
 pub type Ashift = Property<AshiftSectorSize>;
-pub type Autoexpand = Property<YesNo>;
-pub type Autoreplace = Property<YesNo>;
-pub type Autotrim = Property<YesNo>;
+pub type Autoexpand = Property<OnOff>;
+pub type Autoreplace = Property<OnOff>;
+pub type Autotrim = Property<OnOff>;
 pub type Bootfs = Property<String>; // TODO: check if it really string
 pub type Cachefile = Property<String>; // TODO: check if it really string
 pub type Capacity = Property<u64>;
 pub type Comment = Property<String>;
-pub type Delegation = Property<YesNo>;
+pub type Delegation = Property<OnOff>;
 pub type Dedupditto = Property<u64>;
 pub type Expandsize = Property<u64>;
 pub type Failmode = Property<FailmodeStatus>;
@@ -33,6 +32,6 @@ pub type Health = Property<HealthStatus>;
 pub type Listsnapshots = Property<OnOff>;
 pub type Loadguid = Property<u64>;
 pub type Multihost = Property<OnOff>;
-pub type Readonly = Property<YesNo>;
+pub type Readonly = Property<OnOff>;
 pub type Size = Property<u64>;
 pub type Version = Property<u64>;
