99913a751f9f87cf0d496077bfadbcee4c82125f
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-07-07 11:48:49 +0000

    refactored bootfs to enum and not option

--- src/zfs/zpool_property/bootfs.rs
+++ src/zfs/zpool_property/bootfs.rs
@@ -1,17 +1,18 @@
 use std::fmt;
-use std::ops;
 use std::str::FromStr;
 
 use serde::{Deserialize, Serialize};
-
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Bootfs(Option<String>);
+pub enum Bootfs {
+    BootableDataset(String),
+    Empty,
+}
 
 impl fmt::Display for Bootfs {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.0 {
-            Some(x) => x.fmt(f),
-            None => "-".fmt(f),
+        match self {
+            Self::BootableDataset(dataset) => dataset.fmt(f),
+            Self::Empty => "-".fmt(f),
         }
     }
 }
@@ -21,16 +22,8 @@ impl FromStr for Bootfs {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "-" => Ok(Self(None)),
-            dataset => Ok(Self(Some(dataset.to_string()))),
+            "-" => Ok(Self::Empty),
+            dataset => Ok(Self::BootableDataset(dataset.to_string())),
         }
-    }
-}
-
-impl ops::Deref for Bootfs {
-    type Target = String;
-
-    fn deref(&self) -> &Self::Target {
-        self.0.as_ref().unwrap()
     }
 }
