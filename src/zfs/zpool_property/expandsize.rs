ba348f58f7aa031527a46501e8f1e73807bf1dc7
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-07-07 11:54:41 +0000

    refactored expandsize property to use enum and not option

--- src/zfs/zpool_property/expandsize.rs
+++ src/zfs/zpool_property/expandsize.rs
@@ -1,17 +1,19 @@
 use std::fmt;
-use std::ops;
 use std::str::FromStr;
 
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize)]
-pub struct Expandsize(Option<u64>);
+pub enum Expandsize {
+    Size(u64),
+    Empty,
+}
 
 impl fmt::Display for Expandsize {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match &self.0 {
-            Some(x) => x.fmt(f),
-            None => "-".fmt(f),
+        match self {
+            Self::Size(size) => size.fmt(f),
+            Self::Empty => "-".fmt(f),
         }
     }
 }
@@ -21,19 +23,11 @@ impl FromStr for Expandsize {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "-" => Ok(Self(None)),
+            "-" => Ok(Self::Empty),
             other => other
                 .parse::<u64>()
                 .map_err(|_| super::InvalidProperty::invalid_value(s))
-                .map(|size| Self(Some(size))),
+                .map(|size| Self::Size(size)),
         }
-    }
-}
-
-impl ops::Deref for Expandsize {
-    type Target = u64;
-
-    fn deref(&self) -> &Self::Target {
-        self.0.as_ref().unwrap()
     }
 }
