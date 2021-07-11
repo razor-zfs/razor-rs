793286a0eb28445878c454dd6acedf4a044e14c0
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-07-11 13:46:27 +0000

    added zfs core FFI

--- zfs_core-sys/build.rs
+++ zfs_core-sys/build.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 fn main() {
     // Tell cargo to tell rustc to link the system nvpair of zfs
     // shared library.
-    println!("cargo:rustc-link-lib=nvpair");
+    println!("cargo:rustc-link-lib=zfs_core");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -13,11 +13,13 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
+        .blocklist_item(r#"(\w*nvpair\w*)"#)
+        .blocklist_type(r#"(\w*nvpair\w*)"#)
+        .blocklist_function(r#"(\w*nvpair\w*)"#)
+        .blocklist_item(r#"(\w*nvlist\w*)"#)
+        .blocklist_type(r#"(\w*nvlist\w*)"#)
+        .blocklist_function(r#"(\w*nvlist\w*)"#)
         .clang_args(vec!["-I/usr/include/libzfs", "-I/usr/include/libspl"])
-        .allowlist_type(r#"(\w*nvpair\w*)"#)
-        .allowlist_type(r#"(\w*nvpair\w*)"#)
-        .allowlist_function(r#"(\w*nvpair\w*)"#)
-        .allowlist_function(r#"(\w*nvpair\w*)"#)
         .default_enum_style(bindgen::EnumVariation::Rust {
             non_exhaustive: (true),
         })
@@ -29,6 +31,6 @@ fn main() {
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("nvpair.rs"))
+        .write_to_file(out_path.join("zfs_core.rs"))
         .expect("Couldn't write bindings!");
 }
