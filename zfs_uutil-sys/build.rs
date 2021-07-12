c7b0378cac33385264c9e3fb336ba123ec56f218
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-07-12 08:04:51 +0000

    added zfs uutil FFI

--- zfs_uutil-sys/build.rs
+++ zfs_uutil-sys/build.rs
@@ -4,7 +4,7 @@ use std::path::PathBuf;
 fn main() {
     // Tell cargo to tell rustc to link the system nvpair of zfs
     // shared library.
-    println!("cargo:rustc-link-lib=zfs_core");
+    println!("cargo:rustc-link-lib=uutil");
 
     // The bindgen::Builder is the main entry point
     // to bindgen, and lets you build up options for
@@ -13,13 +13,9 @@ fn main() {
         // The input header we would like to generate
         // bindings for.
         .header("wrapper.h")
-        .allowlist_var(r#"(\w*lzc\w*)"#)
-        .allowlist_type(r#"(\w*lzc\w*)"#)
-        .allowlist_function(r#"(\w*lzc\w*)"#)
-        .allowlist_function(r#"(\w*libzfs\w*)"#)
-        .blocklist_item(r#"(\w*nvlist\w*)"#)
-        .blocklist_type(r#"(\w*nvlist\w*)"#)
-        .blocklist_function(r#"(\w*nvlist\w*)"#)
+        .allowlist_var(r#"(\w*uu\w*)"#)
+        .allowlist_type(r#"(\w*uu\w*)"#)
+        .allowlist_function(r#"(\w*uu\w*)"#)
         .clang_args(vec!["-I/usr/include/libzfs", "-I/usr/include/libspl"])
         .default_enum_style(bindgen::EnumVariation::Rust {
             non_exhaustive: (true),
@@ -32,6 +28,6 @@ fn main() {
     // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings
-        .write_to_file(out_path.join("zfs_core.rs"))
+        .write_to_file(out_path.join("uutil.rs"))
         .expect("Couldn't write bindings!");
 }
