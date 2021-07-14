74b09535f3ce9c16a3af7118542bcabd212801e3
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-07-14 13:39:21 +0000

    added nvlist functionality

--- razorzfsnvpair/src/library/nvlist/impls.rs
+++ razorzfsnvpair/src/library/nvlist/impls.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-macro_rules! generate_func_call {
+macro_rules! basic_nvlist_functions {
     ($(($name: ident, $mytype: ty, $callback: path)),*) => {
         impl NvList {
             $(
@@ -15,7 +15,23 @@ macro_rules! generate_func_call {
     };
 }
 
-generate_func_call!(
+macro_rules! vector_nvlist_functions {
+    ($(($name: ident, $mytype: ty, $callback: path)),*) => {
+        impl NvList {
+            $(
+                fn $name<T: Pair<Value = $mytype>>(&mut self, nvp: T) {
+                    let name = nvp.key().as_ptr();
+                    let value = &*nvp.value();
+                    let mut value = value.clone();
+
+                    unsafe { $callback(self.raw, name, value.as_mut_ptr(), value.len() as u32) };
+                }
+            )*
+        }
+    };
+}
+
+basic_nvlist_functions!(
     (add_u8, u8, sys::nvlist_add_uint8),
     (add_u16, u16, sys::nvlist_add_uint16),
     (add_u32, u32, sys::nvlist_add_uint32),
@@ -25,4 +41,15 @@ generate_func_call!(
     (add_i32, i32, sys::nvlist_add_int32),
     (add_i64, i64, sys::nvlist_add_int64),
     (add_bool, sys::boolean_t, sys::nvlist_add_boolean_value)
+);
+
+vector_nvlist_functions!(
+    (add_u8_arr, Vec<u8>, sys::nvlist_add_uint8_array),
+    (add_u16_arr, Vec<u16>, sys::nvlist_add_uint16_array),
+    (add_u132_arr, Vec<u32>, sys::nvlist_add_uint32_array),
+    (add_u64_arr, Vec<u64>, sys::nvlist_add_uint64_array),
+    (add_i8_arr, Vec<i8>, sys::nvlist_add_int8_array),
+    (add_i16_arr, Vec<i16>, sys::nvlist_add_int16_array),
+    (add_i132_arr, Vec<i32>, sys::nvlist_add_int32_array),
+    (add_i64_arr, Vec<i64>, sys::nvlist_add_int64_array)
 );
