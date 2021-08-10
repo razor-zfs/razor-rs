9e2c55761917277c29917bcd67f784f4fb658e05
Cyril Plisko <cyril.plisko@replix.io>
2021-08-10 14:32:11 +0000

    nvpair: Add lint preambule

--- nvpair/src/lib.rs
+++ nvpair/src/lib.rs
@@ -1,3 +1,18 @@
+#![cfg_attr(feature = "pedantic", warn(clippy::pedantic))]
+#![warn(clippy::use_self)]
+#![warn(clippy::map_flatten)]
+#![warn(clippy::map_unwrap_or)]
+#![warn(deprecated_in_future)]
+#![warn(future_incompatible)]
+#![warn(noop_method_call)]
+#![warn(unreachable_pub)]
+#![warn(missing_debug_implementations)]
+#![warn(rust_2018_compatibility)]
+#![warn(rust_2021_compatibility)]
+#![warn(rust_2018_idioms)]
+#![warn(unused)]
+#![deny(warnings)]
+
 use razor_nvpair_sys as sys;
 
 pub use error::NvListError;
