a826d94c52c9222f01948272f8eb45c797df573c
Andrey Obruchkov <andrey.obruchkov@replix.io>
2021-09-01 06:46:13 +0000

    changed ZfsHandler to LibZfsHandler

--- zfscore/src/zfs_handler.rs
+++ zfscore/src/zfs_handler.rs
@@ -2,21 +2,21 @@ use super::sys;
 use once_cell::sync::Lazy;
 use parking_lot::Mutex;
 
-pub(crate) static ZFS_HANDLER: Lazy<Mutex<ZfsHandler>> = Lazy::new(|| {
-    let handler = ZfsHandler::init();
+pub(crate) static ZFS_HANDLER: Lazy<Mutex<LibZfsHandler>> = Lazy::new(|| {
+    let handler = LibZfsHandler::init();
     Mutex::new(handler)
 });
 
 #[derive(Debug)]
-pub(crate) struct ZfsHandler {
+pub(crate) struct LibZfsHandler {
     raw_libzfs_handle: *mut sys::libzfs_handle_t,
 }
 
 // TODO: check this!!
-unsafe impl Send for ZfsHandler {}
-unsafe impl Sync for ZfsHandler {}
+unsafe impl Send for LibZfsHandler {}
+unsafe impl Sync for LibZfsHandler {}
 
-impl ZfsHandler {
+impl LibZfsHandler {
     fn init() -> Self {
         dbg!("initializing zfs handler");
         Self {
@@ -30,7 +30,7 @@ impl ZfsHandler {
 }
 
 // TODO: check how to free zfs_handle_t
-impl Drop for ZfsHandler {
+impl Drop for LibZfsHandler {
     fn drop(&mut self) {
         unsafe { sys::libzfs_fini(self.raw_libzfs_handle) }
     }
