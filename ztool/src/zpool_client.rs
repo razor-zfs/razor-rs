3f240cbbf141a2e16e3f1d62d694ef298469e51b
David <david.avdat@replix.io>
2021-10-07 09:04:56 +0000

    Do not include root dev partition

--- ztool/src/zpool_client.rs
+++ ztool/src/zpool_client.rs
@@ -2,7 +2,7 @@ pub(crate) use client::{Method, Property};
 use razor_zfsrpc::zpool_client as client;
 
 use tokio::process::Command;
-use walkdir::WalkDir;
+use walkdir::{DirEntry, WalkDir};
 
 #[allow(unused)]
 use tracing::{debug, error, info, trace, warn};
@@ -20,8 +20,29 @@ pub(crate) struct Client {
 
 impl Client {
     const DEVICES_PATH: &'static str = "/replixio/dev/disk/by-id";
-    const EBS: &'static str = "Amazon_Elastic_Block_Store";
-    const ROOT_DEV: &'static str = "nvme0n1";
+    const EBS_PREFIX: &'static str = "nvme-Amazon_Elastic_Block_Store";
+    const ROOT_DEV_PREFIX: &'static str = "nvme0";
+
+    fn is_ebs_device(dev: &DirEntry) -> bool {
+        dev.file_name()
+            .to_str()
+            .map_or(false, |s| s.starts_with(Self::EBS_PREFIX))
+    }
+
+    fn is_root_device(dev: &DirEntry) -> bool {
+        let f = || {
+            dev.path()
+                .canonicalize()
+                .ok()?
+                .as_path()
+                .file_name()?
+                .to_str()?
+                .starts_with(Self::ROOT_DEV_PREFIX)
+                .then(|| ())
+        };
+
+        f().is_some()
+    }
 
     pub(crate) async fn new(port: String) -> Self {
         let inner = client::Client::new(port).await;
@@ -53,15 +74,15 @@ impl Client {
 
         debug!(?vendor);
 
-        let mut disks = vec![];
         let disks = match vendor {
             Vendor::Azure => {
-                disks.push("/replixio/dev/disk/azure/scsi1/lun2".into());
-                disks.push("/replixio/dev/disk/azure/scsi1/lun3".into());
-                disks.push("/replixio/dev/disk/azure/scsi1/lun4".into());
-                disks.push("/replixio/dev/disk/azure/scsi1/lun5".into());
-                disks.push("/replixio/dev/disk/azure/scsi1/lun6".into());
-                disks
+                vec![
+                    "/replixio/dev/disk/azure/scsi1/lun2".into(),
+                    "/replixio/dev/disk/azure/scsi1/lun3".into(),
+                    "/replixio/dev/disk/azure/scsi1/lun4".into(),
+                    "/replixio/dev/disk/azure/scsi1/lun5".into(),
+                    "/replixio/dev/disk/azure/scsi1/lun6".into(),
+                ]
             }
 
             Vendor::Aws => {
@@ -69,20 +90,15 @@ impl Client {
                 // disks
                 WalkDir::new(Self::DEVICES_PATH)
                     .into_iter()
-                    .fold(disks, |mut disks, entry| {
-                        let entry = entry.unwrap();
-                        if entry.path().to_str().unwrap().contains(Self::EBS)
-                            && !entry
-                                .path()
-                                .canonicalize()
-                                .unwrap()
-                                .ends_with(Self::ROOT_DEV)
-                        {
-                            let disk = entry.path().to_str().unwrap().to_string();
-                            disks.push(disk);
-                        }
-                        disks
+                    .filter_entry(|entry| {
+                        Self::is_ebs_device(entry) && !Self::is_root_device(entry)
                     })
+                    .filter_map(|entry| {
+                        entry
+                            .ok()
+                            .and_then(|entry| entry.path().to_str().map(|entry| entry.to_string()))
+                    })
+                    .collect()
             }
         };
 
