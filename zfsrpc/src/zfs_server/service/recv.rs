8e2d05ef3766a85716861c385a75ce130bde67c6
Cyril Plisko <cyril.plisko@statehub.io>
2022-03-15 12:36:14 +0000

    service/recv.rs: cleanup

--- zfsrpc/src/zfs_server/service/recv.rs
+++ zfsrpc/src/zfs_server/service/recv.rs
@@ -1,11 +1,6 @@
-// use std::pin::Pin;
-
 use tokio::io::AsyncWriteExt;
 use tokio_pipe::pipe;
-use tonic::Code;
 use tonic::Status;
-// use tokio_stream::Stream;
-// use tokio_util::io::ReaderStream;
 
 use super::*;
 
@@ -19,22 +14,25 @@ pub async fn recv(mut input: tonic::Streaming<proto::SendSegment>) -> ZfsRpcResu
     };
 
     let snapname = segment.name;
-    let mut sequence = segment.sequence;
-    debug!("Receiving {sequence}");
+    let mut expected_sequence = segment.sequence + 1;
+    debug!(sequence = segment.sequence, "Receiving message");
 
     let (reader, mut writer) = pipe()?;
     let receiver = task::spawn_blocking(|| zfs::Zfs::receive(snapname, origin, false, reader));
     writer.write_all(&segment.buffer).await?;
 
     while let Some(segment) = input.message().await? {
-        if sequence + 1 != segment.sequence {
-            let cur_sequence = segment.sequence;
-            let message = format!("Receiving {cur_sequence} while previous was {sequence:?}");
-            return Err(Status::new(Code::InvalidArgument, message));
+        debug!(sequence = segment.sequence, "Receiving message");
+        if expected_sequence == segment.sequence {
+            expected_sequence = segment.sequence + 1;
+        } else {
+            let message = format!(
+                "Message sequence mismatch: received {}, expected {}",
+                segment.sequence, expected_sequence
+            );
+            return Err(Status::invalid_argument(message));
         }
 
-        sequence = segment.sequence;
-        debug!("Receiving {sequence:?}");
         writer.write_all(&segment.buffer).await?;
     }
 
@@ -45,40 +43,3 @@ pub async fn recv(mut input: tonic::Streaming<proto::SendSegment>) -> ZfsRpcResu
 
     Ok(response)
 }
-
-// pub type SendStream = Pin<Box<dyn Stream<Item = Result<proto::SendSegment, tonic::Status>> + Send>>;
-
-// impl proto::SendRequest {
-//     pub async fn execute(self) -> ZfsRpcResult<SendStream> {
-//         let Self { from, source } = self;
-//         let from = if from.is_empty() { None } else { Some(from) };
-//         let name = source.clone();
-//         let (mut reader, writer) = pipe()?;
-//         let sender = task::spawn_blocking(|| zfs::Zfs::send(source, from, writer));
-
-//         let send_stream = async_stream::try_stream! {
-//             let mut sequence = 0;
-//             let mut _send_complete = false;
-//             loop {
-//                 let mut buffer = [0; 128 * 1024];
-//                 let count = reader.read(&mut buffer).await?;
-//                 if count > 0 {
-//                     let segment = proto::SendSegment {
-//                             name: name.clone(),
-//                             sequence,
-//                             buffer: buffer[0..count].to_vec(),
-//                         };
-//                     yield segment;
-//                 } else {
-//                     break;
-//                 }
-//                 sequence += 1;
-//             }
-//             sender
-//                 .await
-//                 .map_err(join_to_status)?
-//                 .map_err(zfs_to_status)?;
-//         };
-//         Ok(Response::new(Box::pin(send_stream)))
-//     }
-// }
