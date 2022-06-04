3faf60ba880bde386f0bdcbb6c246f9a1a4658b0
Cyril Plisko <cyril.plisko@mountall.com>
2022-06-04 14:02:38 +0300

    libzfscore-sys: cleanup

--- libzfscore-sys/src/lib.rs
+++ libzfscore-sys/src/lib.rs
@@ -1,8 +1,5 @@
-#![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
 #![allow(deref_nullptr)]
-#![allow(clippy::return_self_not_must_use)]
 
 pub use razor_libnvpair::*;
 
