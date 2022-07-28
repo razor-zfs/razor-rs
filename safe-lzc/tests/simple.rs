d8e472c0e255a2030cab19848e38d2536c5a6605
Cyril Plisko <cyril.plisko@mountall.com>
2022-07-28 14:26:51 +0300

    safe-lzc: pass props reference in tests

--- safe-lzc/tests/simple.rs
+++ safe-lzc/tests/simple.rs
@@ -34,11 +34,10 @@ fn filesystem_create_existing() {
     let name = dbg!(namespace.unique_name());
     let mut props = nvpair::NvList::new();
     props += ("razor-test:clean", "yes");
-    lzc::create_filesystem(&name, props).unwrap();
+    lzc::create_filesystem(&name, &props).unwrap();
     assert!(lzc::dataset_exists(&name));
-    let mut props = nvpair::NvList::new();
-    props += ("razor-test:clean", "yes");
-    lzc::create_filesystem(&name, props).unwrap_err();
+    let e = lzc::create_filesystem(&name, props).unwrap_err();
+    assert_eq!(e.code, libc::EEXIST);
     lzc::destroy_dataset(&name).unwrap();
     assert!(!lzc::dataset_exists(&name));
 }
@@ -51,13 +50,10 @@ fn zvol_create_existing() {
     props += ("razor-test:clean", "yes");
     props += ("volsize", 1024_u64 * 1024 * 1024);
     props += ("volblocksize", 4096_u64);
-    lzc::create_volume(&name, props).unwrap();
+    lzc::create_volume(&name, &props).unwrap();
     assert!(lzc::dataset_exists(&name));
-    let mut props = nvpair::NvList::new();
-    props += ("razor-test:clean", "yes");
-    props += ("volsize", 1024_u64 * 1024 * 1024);
-    props += ("volblocksize", 4096_u64);
-    lzc::create_volume(&name, props).unwrap_err();
+    let e = lzc::create_volume(&name, props).unwrap_err();
+    assert_eq!(e.code, libc::EEXIST);
     lzc::destroy_dataset(&name).unwrap();
     assert!(!lzc::dataset_exists(&name));
 }
