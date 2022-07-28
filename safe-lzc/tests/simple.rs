6069535263d8c0cfd71e383b4da3413c1ba07d23
Cyril Plisko <cyril.plisko@mountall.com>
2022-07-28 11:00:04 +0300

    safe-lzc: use razor-test

--- safe-lzc/tests/simple.rs
+++ safe-lzc/tests/simple.rs
@@ -1,29 +1,6 @@
-use std::env;
-
-use razor_test::TestNamespace;
-
-use nanoid::nanoid;
-use nanoid_dictionary::ALPHANUMERIC;
-
 use razor_nvpair as nvpair;
 use razor_safe_lzc as lzc;
-
-struct LzcTestNamespace {
-    parent: String,
-}
-
-impl LzcTestNamespace {
-    fn new() -> Self {
-        let parent = env::var("RAZOR_TEST")
-            .ok()
-            .unwrap_or_else(|| "rpool/razor-test".to_string());
-        Self { parent }
-    }
-
-    fn name(&self) -> String {
-        format!("{}/{}", self.parent, nanoid!(8, ALPHANUMERIC))
-    }
-}
+use razor_test::TestNamespace;
 
 #[test]
 fn filesystem_create_destroy() {
@@ -39,14 +16,48 @@ fn filesystem_create_destroy() {
 
 #[test]
 fn zvol_create_destroy() {
-    let namespace = LzcTestNamespace::new();
-    let name = dbg!(namespace.name());
+    let namespace = TestNamespace::unique();
+    let name = dbg!(namespace.unique_name());
     let mut props = nvpair::NvList::new();
     props += ("razor-test:clean", "yes");
     props += ("volsize", 1024_u64 * 1024 * 1024);
     props += ("volblocksize", 4096_u64);
     lzc::create_volume(&name, props).unwrap();
     assert!(lzc::dataset_exists(&name));
+    lzc::destroy_dataset(&name).unwrap();
+    assert!(!lzc::dataset_exists(&name));
+}
+
+#[test]
+fn filesystem_create_existing() {
+    let namespace = TestNamespace::unique();
+    let name = dbg!(namespace.unique_name());
+    let mut props = nvpair::NvList::new();
+    props += ("razor-test:clean", "yes");
+    lzc::create_filesystem(&name, props).unwrap();
+    assert!(lzc::dataset_exists(&name));
+    let mut props = nvpair::NvList::new();
+    props += ("razor-test:clean", "yes");
+    lzc::create_filesystem(&name, props).unwrap_err();
+    lzc::destroy_dataset(&name).unwrap();
+    assert!(!lzc::dataset_exists(&name));
+}
+
+#[test]
+fn zvol_create_existing() {
+    let namespace = TestNamespace::unique();
+    let name = dbg!(namespace.unique_name());
+    let mut props = nvpair::NvList::new();
+    props += ("razor-test:clean", "yes");
+    props += ("volsize", 1024_u64 * 1024 * 1024);
+    props += ("volblocksize", 4096_u64);
+    lzc::create_volume(&name, props).unwrap();
+    assert!(lzc::dataset_exists(&name));
+    let mut props = nvpair::NvList::new();
+    props += ("razor-test:clean", "yes");
+    props += ("volsize", 1024_u64 * 1024 * 1024);
+    props += ("volblocksize", 4096_u64);
+    lzc::create_volume(&name, props).unwrap_err();
     lzc::destroy_dataset(&name).unwrap();
     assert!(!lzc::dataset_exists(&name));
 }
