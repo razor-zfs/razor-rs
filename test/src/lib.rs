42a43ef0aa124876896cff8a2324f3c07e499ea0
Cyril Plisko <cyril.plisko@mountall.com>
2022-07-28 18:12:03 +0300

    zfs: migrate tests to razor-test

--- test/src/lib.rs
+++ test/src/lib.rs
@@ -10,8 +10,6 @@ use std::time::Duration;
 use nanoid::nanoid;
 use nanoid_dictionary::ALPHANUMERIC;
 
-// use razor_libzfscore::error::CoreError;
-// use razor_libzfscore::zfs_type_t;
 use razor_safe_lzc as lzc;
 use razor_zfs as zfs;
 
